95db274b6b03faac3a7eb064079d34de9db0a225
MUY SENGLY <muysengly@gmail.com>
2026-09-17 15:15:10 +0700

    2026-09-17 15:15:10

---
+++ lib/core/variable.dart
@@ -0,0 +1,16 @@
+import "package:flutter/foundation.dart";
+
+class Variable extends ChangeNotifier {
+  static final Variable instance = Variable._();
+  Variable._();
+
+  Future<void> init() async {
+    notifyListeners();
+  }
+
+  void notify() {
+    notifyListeners();
+  }
+}
+
+Variable variable = Variable.instance;
